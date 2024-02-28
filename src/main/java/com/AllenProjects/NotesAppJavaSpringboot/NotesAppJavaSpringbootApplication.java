49f22d13f452980df9eaf86da2c47a6266552b6c
Allenmac2811 <80910935+Allenmac2811@users.noreply.github.com>
2024-02-27 19:55:16 -0500

    Initial commit

---
+++ src/main/java/com/AllenProjects/NotesAppJavaSpringboot/NotesAppJavaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.AllenProjects.NotesAppJavaSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesAppJavaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesAppJavaSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AllenProjects/NotesAppJavaSpringboot/NotesAppJavaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AllenProjects.NotesAppJavaSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesAppJavaSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
